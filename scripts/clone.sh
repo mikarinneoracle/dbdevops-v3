ee2e94f5f586676fa24667eee83e41289b850694
Mika Rinne <mika.rinne@oracle.com>
2021-11-09 10:40:32 +0200

    create

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -32,16 +32,14 @@ git config --global user.email $git_user
 git config --global user.name $git_email
 git checkout -b $timestamp-copy-prod
 
-echo "*** CREATES PROD COPY BRANCH TO REPO ***"
+echo "*** CREATES $timestamp-copy-prod BRANCH TO REPO ***"
 
 printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@prod_high\ntables\nlb genschema -split\n${tablesconfig}\nexit" > gen.sql
-cat gen.sql
 sql /nolog @./gen.sql
 rm -f gen.sql
 
 if [ -n "${application_id}" ] &&  [ "${application_id}" != "-" ]; then
     printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@prod_high\ntables\nlb genobject -type apex -applicationid ${application_id} -skipExportDate -expOriginalIds\nexit" > gen_apex.sql
-    cat gen_apex.sql
     sql /nolog @./gen_apex.sql
     rm -f gen_apex.sql
 fi
