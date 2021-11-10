1c2d64247e52cc2d8e298b2854ad57dddcc93e00
Mika Rinne <mika.rinne@oracle.com>
2021-11-10 16:12:12 +0200

    create

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -25,13 +25,13 @@ wget $prod_db_wallet_preauth -O wallet.zip
 
 echo "*** MERGE REPO MASTER TO ${name} ***"
 
-printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@prod_high\nlb update -changelog controller.xml\nlb update -changelog data.xml\ntables\nexit" > upd.sql
+printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@${name}_high\nlb update -changelog controller.xml\nlb update -changelog data.xml\ntables\nexit" > upd.sql
 
 sql /nolog @./upd.sql
 rm -f upd.sql
 
 if [ -n "${application_id}" ]; then
-    printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@prod_high\n@privileges.sql\nlb update -changelog f${application_id}.xml\nexit" > upd_apex.sql
+    printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@${name}_high\n@privileges.sql\nlb update -changelog f${application_id}.xml\nexit" > upd_apex.sql
     sql /nolog @./upd_apex.sql
     rm -f upd_apex.sql
 fi
