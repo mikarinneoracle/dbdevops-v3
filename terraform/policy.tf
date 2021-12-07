4da5ef21f7f965d2a3e005e1cd520a1e209d3ded
Mika Rinne <mika.rinne@oracle.com>
2021-12-07 14:35:16 +0200

    policy tenant -> compartment

--- terraform/policy.tf
+++ terraform/policy.tf
@@ -1,6 +1,6 @@
 resource "oci_identity_policy" "devops_allow_object_storage_lifecycle" {
   name           = "devops-object-family-${var.task_id}"
   description    = "policy created by terraform for devops"
-  compartment_id = var.tenancy_ocid
+  compartment_id = var.compartment_ocid
   statements     = ["Allow service objectstorage-${var.region} to manage object-family in tenancy"]
 }
