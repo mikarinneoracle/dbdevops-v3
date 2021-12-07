80cd0e317709f6f53f65221a725b1f6e30ea6df0
Mika Rinne <mika.rinne@oracle.com>
2021-12-07 09:50:16 +0200

    merge.sh privs update

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -51,12 +51,21 @@ fi
 printf "\ntables\nexit" >> upd.sql
 
 sql /nolog @./upd.sql
-rm -f upd.sql
-
+rm -f upd.sql        
+        
 if [ -n "${application_id}" ]; then
     if [ -f "f${application_id}.xml" ]; then
-        printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@${name}_high\nlb update -changelog f${application_id}.xml\nexit" > upd_apex.sql
+        echo "Copying application ${application_id} to ${prod_instance_name}/${schema}."
+        printf "declare\nl_workspace_id number;\nbegin\n" > upd_apex_privs.sql;
+        printf "l_workspace_id := apex_util.find_security_group_id (p_workspace => 'WORKSPACE_NAME');\n" >> upd_apex_privs.sql;
+        printf "apex_util.set_security_group_id (p_security_group_id => l_workspace_id);\n" >> upd_apex_privs.sql;
+        printf "APEX_UTIL.PAUSE(2);\n" >> upd_apex_privs.sql;
+        printf "end;\n/\n" >> upd_apex_privs.sql;
+        
+        printf "set cloudconfig ./wallet.zip\nconn ${schema}/${pwd}@${name}_high\n@upd_apex_privs.sql\nlb update -changelog f${application_id}.xml\nexit" > upd_apex.sql
+        
         sql /nolog @./upd_apex.sql
+        rm -f upd_apex.sql
         rm -f upd_apex.sql
     else
         echo "f${application_id}.xml not found. App not copied to ${prod_instance_name}."
