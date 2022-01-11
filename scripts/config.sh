9bdc25dd146a32fa1dd4fcc41d5b61679a42c032
Mika Rinne <mika.rinne@oracle.com>
2022-01-11 12:48:03 +0200

    db name

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,13 +6,7 @@ read -p "Region name: " region
 
 read -p "Compartment OCID: " compartment
 
-read -p "Production database instance name: " name
-
-read -p "Production instance $name database name (if not $name) : " dbname
-
-if [ -z $dbname ]; then
-   export dbname=$name
-fi
+read -p "Production database name: " name
 
 read -p "Development database instance name prefix (e.g. Dev): " dev_name
 
@@ -29,6 +23,6 @@ if [ -f "settings.env" ]; then
     cp settings.env settings.env.backup 
 fi
 
-printf "prod_instance_name=${name}\nprod_dbname=${dbname}\nprod_db_wallet_preauth=${preauth}\ntenancy_ocid=${tenancy}\nregion=${region}\ncompartment_ocid=${compartment}\nos_bucket_tf=${bucket}\ndev_db_name=${dev_name}\n" > settings.env
+printf "prod_instance_name=${name}\nprod_db_wallet_preauth=${preauth}\ntenancy_ocid=${tenancy}\nregion=${region}\ncompartment_ocid=${compartment}\nos_bucket_tf=${bucket}\ndev_db_name=${dev_name}\n" > settings.env
 
 echo "Config saved to settings.env"
